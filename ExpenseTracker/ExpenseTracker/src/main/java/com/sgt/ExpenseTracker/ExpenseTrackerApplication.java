865767b07006068113f9e6207109977b63f485dd
chiragkrishnani13 <chiragkrishnani13@gmail.com>
2026-01-03 20:38:52 +0530

    tested the dependency

--- ExpenseTracker/ExpenseTracker/src/main/java/com/sgt/ExpenseTracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/ExpenseTracker/src/main/java/com/sgt/ExpenseTracker/ExpenseTrackerApplication.java
@@ -1,9 +1,8 @@
 package com.sgt.ExpenseTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication()
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
--- ExpenseTracker/ExpenseTracker/src/test/java/com/sgt/ExpenseTracker/service/AuthServiceTest.java
+++ ExpenseTracker/ExpenseTracker/src/test/java/com/sgt/ExpenseTracker/service/AuthServiceTest.java
@@ -1,11 +1,13 @@
 package com.sgt.ExpenseTracker.service;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
 class AuthServiceTest {
-
-
+    @Test
+    public void testPassword(){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(bCryptPasswordEncoder.encode("password"));
+    }
+  
 }
